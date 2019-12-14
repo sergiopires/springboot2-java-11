7d511d8b9a105ae6dd6f50f9d29974850d8b4cb1
sergio.ramos <sergiocop1@hotmail.com>
2019-12-14 18:18:20 -0300

    Criação do projeto

---
+++ curso/src/test/java/com/sergioramos/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergioramos.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
