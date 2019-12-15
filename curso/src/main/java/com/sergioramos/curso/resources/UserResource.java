89fd5e32a17e2881c3f11b0c78a87d39efc2e267
sergio.ramos <sergiocop1@hotmail.com>
2019-12-14 23:01:20 -0300

    Configuração /h2, profile

--- curso/src/main/java/com/sergioramos/curso/resources/UserResource.java
+++ curso/src/main/java/com/sergioramos/curso/resources/UserResource.java
@@ -1,5 +1,7 @@
 package com.sergioramos.curso.resources;
 
+
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
